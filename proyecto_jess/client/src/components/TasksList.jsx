d9d259cb6fae39afd71cd2c3fd9acb6e29a28cc7
Jesica Gutiérrez <jesica.gutierrez5@soy.sena.edu.co>
2024-06-27 14:37:31 -0500

    Django

---
+++ proyecto_jess/client/src/components/TasksList.jsx
@@ -0,0 +1,21 @@
+import { useEffect, useState } from "react";
+import { getAllTask } from "../api/tasks.api";
+import { TaskCard } from "./TaskCard";
+export function TasksList() {
+    const [tasks, setTasks] = useState([]);
+
+    useEffect (() => {
+        async function loadTasks() {
+            const res = await getAllTask();
+            console.log(res);
+            setTasks(res.data);
+        }
+        loadTasks();
+    }, []);
+
+    return <div className="grid grid-cols-3 gap-3">
+        {tasks.map(task => (
+            <TaskCard key={task.id} task={task}/>
+        ))}
+    </div>
+}
